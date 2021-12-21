23461e08f1432cb8dd9369f70f30a429f6d7372f
Cobb-Yoo <yhj010597@naver.com>
2021-12-21 21:46:11 +0900

    css

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.listen();
+app.listen(8080, function () {
+  console.log("hello node");
+});
+
+//app.get('경로', function(req, res))
+
+app.get("/test2", (req, res) => {
+  b.send("ㅋㅋㄹㅃㅃ");
+});
+
+app.get("/test", (reqa, res) => {
+  b.send("ㅋㅋㄹㅃㅃ2");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
