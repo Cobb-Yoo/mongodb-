520e3c486aa9f234592b3192e076379a41e31d5b
Cobb-Yoo <yhj010597@naver.com>
2021-12-22 22:58:19 +0900

    -

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
-  console.log("hello node");
+  console.log("listening on 8080");
 });
 
-//app.get('경로', function(req, res))
-
-app.get("/test2", (req, res) => {
-  b.send("ㅋㅋㄹㅃㅃ");
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
 });
 
-app.get("/test", (reqa, res) => {
-  b.send("ㅋㅋㄹㅃㅃ2");
+app.get("/write", function (요청, 응답) {
+  응답.sendFile(__dirname + "/write.html");
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.post("/add", function (요청, 응답) {
+  console.log(요청.body);
+  응답.send("전송완료");
 });
