b10314eb9fbf2c2c70302a8ffb7196c69a2f1654
Cobb-Yoo <yhj010597@naver.com>
2022-01-02 22:39:56 +0900

    삭제

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ app.get("/list", function (req, res) {
 
 app.delete("/delete", function (req, res) {
   req.body._id = parseInt(req.body._id);
-  db.collection("post").deleteOne(req.body, function () {});
-  console.log(req.body);
+  db.collection("post").deleteOne(req.body, function (err, result) {
+    res.status(200).send({ message: "성공했습니다." });
+  });
 });
